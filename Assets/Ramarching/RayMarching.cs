59e88d0793fbb2509edd17a15ea9ed6978540cb2
Arjen Simons <arjen.simons@gmail.com>
2021-03-02 16:09:17 +0100

    Add start of the RayMarching script

---
+++ Assets/Ramarching/RayMarching.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Raymarching
+{
+    [ExecuteInEditMode, ImageEffectAllowedInSceneView]
+    public class RayMarching : MonoBehaviour
+    {
+        struct ShapeData
+        {
+            public int shapeType;
+            public int blendType;
+            public float blendStrength;
+            public Vector3 color;
+            public Vector3 position;
+            public Vector3 scale;
+
+            public static int GetByteSize() { return sizeof(int) * 2 + sizeof(float) * 10; }
+        }
+
+        [SerializeField] private ComputeShader raymarcher;
+        [SerializeField] private RenderTexture renderTexture;
+        [SerializeField] private Camera cam;
+        [SerializeField] private Light lightSource;
+
+        private ComputeBuffer shapesBuffer;
+    }
+}
